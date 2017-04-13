77dc3a4f3458e1f8acb8f3a68ccc732c3d52c828
vivhp <vivhph03579@fpt.edu.vn>
2017-04-13 15:49:19 +0700

    Commit V3 REACTIVE

--- app/src/main/java/com/xtel/ivipu/presenter/LoginGroupPresenter.java
+++ app/src/main/java/com/xtel/ivipu/presenter/LoginGroupPresenter.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.util.Log;
 
 import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.PhoneNumber;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
@@ -13,9 +14,12 @@ import com.xtel.ivipu.view.activity.ResetActivity;
 import com.xtel.ivipu.view.activity.inf.ILoginGroup;
 import com.xtel.nipservicesdk.CallbackManager;
 import com.xtel.nipservicesdk.callback.CallbacListener;
+import com.xtel.nipservicesdk.callback.CallbackListenerActive;
+import com.xtel.nipservicesdk.callback.CallbackListenerReactive;
 import com.xtel.nipservicesdk.commons.Constants;
 import com.xtel.nipservicesdk.model.entity.Error;
 import com.xtel.nipservicesdk.model.entity.RESP_Login;
+import com.xtel.nipservicesdk.model.entity.RESP_Reactive;
 import com.xtel.nipservicesdk.utils.JsonHelper;
 import com.xtel.nipservicesdk.utils.JsonParse;
 import com.xtel.nipservicesdk.utils.SharedUtils;
@@ -27,17 +31,18 @@ import com.xtel.sdk.commons.NetWorkInfo;
 
 public class LoginGroupPresenter {
 
-    public static int APP_REQUEST_CODE = 100;
-
+    private static final int REACTIVE_REQUEST_CODE = 91;
+    public static int RESET_REQUEST_CODE = 100;
+    String activation_code;
+    String toastMessage;
     private ILoginGroup view;
-
     private String TAG = "Login Group ";
-
     private CallbackManager callbackManager;
 
     public LoginGroupPresenter(ILoginGroup view) {
         this.view = view;
     }
+
 
     public void createCallbackManager() {
         callbackManager = CallbackManager.create(view.getActivity());
@@ -74,15 +79,16 @@ public class LoginGroupPresenter {
                     Log.e("Login err", JsonHelper.toJson(error));
                     String code_err = String.valueOf(error.getCode());
                     int code = Integer.parseInt(code_err);
-                    if (!(code_err == null)) {
-                        view.showShortToast(parseMessage(code));
+                    if (code == 112) {
+                        view.forceActiveAccount();
                     }
+                    view.showShortToast(parseMessage(code));
                 }
             });
         }
     }
 
-    public void onLoginAccountKit() {
+    public void onRequestAccountKit(int type, String phone_number) {
         final Intent intent = new Intent(view.getActivity(), AccountKitActivity.class);
         AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
                 new AccountKitConfiguration.AccountKitConfigurationBuilder(
@@ -90,19 +96,59 @@ public class LoginGroupPresenter {
                         AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
         // ... perform additional configuration ...
         configurationBuilder.setTitleType(AccountKitActivity.TitleType.APP_NAME);
+        configurationBuilder.setInitialPhoneNumber(new PhoneNumber("VN", phone_number));
         configurationBuilder.setDefaultCountryCode("VN");
         intent.putExtra(
                 AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
                 configurationBuilder.build());
-        view.startActivityForResults(intent, APP_REQUEST_CODE);
+        if (type == 1) {
+            view.startActivityForResults(intent, REACTIVE_REQUEST_CODE);
+        } else if (type == 2) {
+            view.startActivityForResults(intent, RESET_REQUEST_CODE);
+        }
     }
+
+
+//    public void onActivityResultAccountKit(final int requestCode,
+//                                           final int resultCode,
+//                                           final Intent data) {
+//        if (requestCode == REACTIVE_REQUEST_CODE) { // confirm that this response matches your request
+//            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+//            String toastMessage;
+//            if (loginResult.getError() != null) {
+//                toastMessage = loginResult.getError().getErrorType().getMessage();
+//            } else if (loginResult.wasCancelled()) {
+//                toastMessage = "Login Cancelled";
+//            } else {
+//                if (loginResult.getAccessToken() != null) {
+//                    toastMessage = "Success:" + loginResult.getAccessToken().getAccountId();
+//                } else {
+//                    toastMessage = String.format(
+//                            "Success:%s...",
+//                            loginResult.getAuthorizationCode().substring(0, 10));
+//                    Log.d("authorization ", loginResult.getAuthorizationCode());
+//                    String authorization = loginResult.getAuthorizationCode();
+//                    forceActiveAccount(authorization);
+//                }
+//
+//                // If you have an authorization code, retrieve it from
+//                // loginResult.getAuthorizationCode()
+//                // and pass it to your server and exchange it for an access token.
+//
+//                // Success! Start your next activity...
+////                goToMyLoggedInActivity();
+//            }
+//
+//            // Surface the result to your user in an appropriate way.
+//            view.showShortToast(toastMessage);
+//        }
+//    }
 
     public void onActivityResultAccountKit(final int requestCode,
                                            final int resultCode,
                                            final Intent data) {
-        if (requestCode == APP_REQUEST_CODE) { // confirm that this response matches your request
+        if (requestCode == RESET_REQUEST_CODE) { // confirm that this response matches your request
             AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
-            String toastMessage;
             if (loginResult.getError() != null) {
                 toastMessage = loginResult.getError().getErrorType().getMessage();
             } else if (loginResult.wasCancelled()) {
@@ -131,6 +177,35 @@ public class LoginGroupPresenter {
 
             // Surface the result to your user in an appropriate way.
             view.showShortToast(toastMessage);
+        } else if (requestCode == REACTIVE_REQUEST_CODE) {
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if (loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+            } else {
+                if (loginResult.getAccessToken() != null) {
+                    toastMessage = "Success:" + loginResult.getAccessToken().getAccountId();
+                } else {
+                    toastMessage = String.format(
+                            "Success:%s...",
+                            loginResult.getAuthorizationCode().substring(0, 10));
+                    Log.d("authorization ", loginResult.getAuthorizationCode());
+                    String authorization = loginResult.getAuthorizationCode();
+                    forceActiveAccount(authorization);
+                }
+
+                // If you have an authorization code, retrieve it from
+                // loginResult.getAuthorizationCode()
+                // and pass it to your server and exchange it for an access token.
+
+                // Success! Start your next activity...
+//                goToMyLoggedInActivity();
+            }
+
+            // Surface the result to your user in an appropriate way.
+            view.showShortToast(toastMessage);
         }
     }
 
@@ -138,4 +213,69 @@ public class LoginGroupPresenter {
         String mess = JsonParse.getCodeMessage(view.getActivity(), code, "");
         return mess;
     }
+
+    public void onReactiveAccount(final String phone_number) {
+        if (!NetWorkInfo.isOnline(view.getActivity())) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    view.onNetworkDisable();
+                }
+            }, 500);
+            return;
+        } else {
+            callbackManager.reactiveNipAccount(phone_number, true, new CallbackListenerReactive() {
+                @Override
+                public void onSuccess(RESP_Reactive reactive) {
+                    activation_code = reactive.getActivation_code();
+                    Log.e("activation code", activation_code);
+                    onAccessAccountKitReactive(phone_number);
+                }
+
+                @Override
+                public void onError(Error error) {
+                    String err_code = String.valueOf(error.getCode());
+                    int code = Integer.parseInt(err_code);
+                    if (!(err_code == null)) {
+                        view.showShortToast(parseMessage(code));
+                    }
+                }
+            });
+        }
+    }
+
+    public void onAccessAccountKitReactive(String phone_number) {
+        final Intent intent = new Intent(view.getActivity(), AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        LoginType.PHONE,
+                        AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
+        // ... perform additional configuration ...
+        configurationBuilder.setTitleType(AccountKitActivity.TitleType.APP_NAME);
+        configurationBuilder.setInitialPhoneNumber(new PhoneNumber("VN", phone_number));
+        configurationBuilder.setDefaultCountryCode("VN");
+        intent.putExtra(
+                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                configurationBuilder.build());
+        view.startActivityForResults(intent, REACTIVE_REQUEST_CODE);
+    }
+
+
+    public void forceActiveAccount(String authorization_code) {
+        String account_type = "PHONE-NUMBER";
+        callbackManager.activeNipAccount(authorization_code, account_type, new CallbackListenerActive() {
+            @Override
+            public void onSuccess() {
+                view.showShortToast("Reactive success!");
+                view.finishActivity();
+            }
+
+            @Override
+            public void onError(Error error) {
+                Log.e("reactive code", String.valueOf(error.getCode()));
+                view.showShortToast("Reactive error code: " + error.getCode());
+            }
+        });
+    }
+
 }
