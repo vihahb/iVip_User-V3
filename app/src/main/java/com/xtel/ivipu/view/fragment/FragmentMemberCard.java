42221dd41b2ff068b2fa77e2ca1d1025cf006fc3
leconglongvu <leconglongvu@gmail.com>
2017-04-17 22:34:53 +0700

    Fix bug fragments overlapping each other

--- app/src/main/java/com/xtel/ivipu/view/fragment/FragmentMemberCard.java
+++ app/src/main/java/com/xtel/ivipu/view/fragment/FragmentMemberCard.java
@@ -200,6 +200,9 @@ public class FragmentMemberCard extends BasicFragment implements DiscreteScrollV
      * Kiểm tra dữ liệu lấy từ trên server về có hay không
      */
     protected void checkListHistory() {
+        progressView.setRefreshing(false);
+        historyAdapter.notifyDataSetChanged();
+
         if (listHistory.size() > 0) {
             progressView.showData();
             historyAdapter.notifyItemChanged(0);
@@ -207,11 +210,6 @@ public class FragmentMemberCard extends BasicFragment implements DiscreteScrollV
             progressView.updateMessage(-1, getString(R.string.message_history_transaction_empty));
             progressView.hideData();
         }
-
-        progressView.setRefreshing(false);
-        historyAdapter.notifyDataSetChanged();
-
-
     }
 
     /**
